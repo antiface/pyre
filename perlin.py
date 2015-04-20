1b1f7f997a8a3ba066ee286b5b6aab0eaaab7528
Nick Hirakawa <nh0815@gmail.com>
2015-04-19 22:33:03 -0400

    Skeleton for perlin functions

---
+++ perlin.py
@@ -0,0 +1,18 @@
+__author__ = 'Nick'
+
+
+# return n-dimensional unit vector
+def getRandomUnitVector(n=3):
+	pass
+
+
+def linearInterpolation():
+	pass
+
+
+def dotProductGridGradient():
+	pass
+
+
+def perlin():
+	pass
